853b8206c7680016dedc69b3dc51a09adbc14ad5
Kevin Burke <kevin@twilio.com>
2013-06-02 10:03:50 -0700

    Format the json and add template fns

--- expectedruns/expectedruns.go
+++ expectedruns/expectedruns.go
@@ -1,12 +1,14 @@
 package expectedruns
 
 import (
-	"appengine"
+	//"appengine"
 	"encoding/json"
 	"fmt"
 	"html/template"
 	"io/ioutil"
 	"net/http"
+	"os"
+	"strconv"
 )
 
 func init() {
@@ -14,7 +16,7 @@ func init() {
 }
 
 type Stat struct {
-	Id            int
+	Id            int // Binary representation of the runners on base
 	ScoringChance float64
 	ExpectedRuns  float64
 }
@@ -28,33 +30,64 @@ type Outs struct {
 	Outs []Out
 }
 
+func checkError(err error) {
+	if err != nil {
+		fmt.Println("Fatal error ", err.Error())
+		os.Exit(1)
+		return
+	}
+}
+
+func addPercent(chance float64) string {
+	return strconv.FormatFloat(chance, 'g', 3, 64) + "%"
+}
+
+func roundDecimal(runs float64) string {
+	return strconv.FormatFloat(runs, 'f', 2, 64)
+}
+
+func onFirstBase(id int) bool {
+	return id&1 == 1
+}
+
+func onSecondBase(id int) bool {
+	return id&2 == 2
+}
+
+func onThirdBase(id int) bool {
+	return id&4 == 4
+}
+
+func startNewRow(id int) bool {
+	return id%2 == 0
+}
+
 func root(w http.ResponseWriter, r *http.Request) {
-	c := appengine.NewContext(r)
+	//c := appengine.NewContext(r)
 
 	var o Outs
 	b, err := ioutil.ReadFile("var/stats.json")
-	if err != nil {
-		fmt.Fprint(w, "there was an error reading the file: "+err.Error())
-		return
-	}
+	checkError(err)
 
 	err = json.Unmarshal(b, &o)
-	if err != nil {
-		fmt.Fprint(w, "there was an error: "+err.Error())
-		return
-	}
+	checkError(err)
+
 	b, err = json.Marshal(o)
-	c.Infof(string(b))
+	checkError(err)
+	//c.Infof(string(b))
 
 	t := template.New("master")
+	t.Funcs(template.FuncMap{
+		"addPercent":   addPercent,
+		"roundDecimal": roundDecimal,
+		"onFirstBase":  onFirstBase,
+		"onSecondBase": onSecondBase,
+		"onThirdBase":  onThirdBase,
+		"startNewRow":  startNewRow,
+	})
 	tpl, err := t.ParseFiles("templates/index.html")
-	if err != nil {
-		fmt.Fprint(w, "there was an error: "+err.Error())
-		return
-	}
-	err = tpl.Execute(w, nil)
-	if err != nil {
-		fmt.Fprint(w, "there was an error: "+err.Error())
-		return
-	}
+	checkError(err)
+
+	err = tpl.Execute(w, o)
+	checkError(err)
 }
